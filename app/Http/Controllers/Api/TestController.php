c1d0dd020b345c57d4f36ac3d5d7df255e735b83
junf <apifan@gmail.com>
2016-02-27 13:53:56 +0800

    init

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return response()->json('Test');
+    }
+}
